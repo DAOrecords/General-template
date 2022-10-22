edd7135a6e964386ee1226066b03ec69b48d453f
Péter Ott <ottpeter@tuta.io>
2022-10-22 10:24:28 +0200

    Adds Red Player

--- src/Main/Player.js
+++ src/Main/Player.js
@@ -41,21 +41,59 @@ const dark = true;
     playerRef.current.pause();
   }
 
+  function prevClicked() {
+    if (selectedSong === 0) {
+      setSelectedSong(list.length-1);
+    } else {
+      setSelectedSong(selectedSong-1);
+    }
+  }
+
+  function nextClicked() {
+    if (selectedSong === list.length-1) {
+      setSelectedSong(0);
+    } else {
+      setSelectedSong(selectedSong+1);
+    }
+  }
+
   function PlayIcon() {
     return (
-      <svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path fillRule="evenodd" ule="evenodd" d="M23.25 18.75L42.75 30L23.25 41.25V18.75Z" fill="#FF0000"/>
+      <svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M23.25 18.75L42.75 30L23.25 41.25V18.75Z" fill={color}/>
+        <circle cx="30" cy="30" r="29.5" stroke={color}/>
       </svg>
     );
   }
   
   function PauseIcon() {
     return(
-      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path fillRule="evenodd" ule="evenodd" d="M10 4H5V20H10V4ZM19 4H14V20H19V4Z" fill="#FF0000"/>
+      <svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M20 16H26V44H20V16Z" fill={color}/>
+        <path d="M34 16H40V44H34V16Z" fill={color}/>
+        <circle cx="30" cy="30" r="29.5" stroke={color}/>
       </svg>
     );
   }
+
+  function PrevIcon() {
+    return (
+      <svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M39 18.75L19.5 30L39 41.25V18.75Z" fill={color}/>
+        <line y1="-1" x2="24" y2="-1" transform="matrix(0 1 1 0 19.25 18)" stroke={color} strokeWidth="2"/>
+      </svg>
+    );
+  }
+
+  function NextIcon() {
+    return (
+      <svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M21 18.75L40.5 30L21 41.25V18.75Z" fill={color}/>
+        <line x1="41.75" y1="18" x2="41.75" y2="42" stroke={color} strokeWidth="2"/>
+      </svg>
+    );
+  }
+
 
   return (
     <div id="musicPlayer">
@@ -65,11 +103,15 @@ const dark = true;
         src={`https://daorecords.io:8443/fetch?cid=${JSON.parse(list[selectedSong].metadata.extra).music_cid}`} 
         ref={playerRef} 
       />
-      {playing? 
-        <button className="musicControlsButton" onClick={pauseClicked}><PauseIcon /></button>
-      : 
-        <button className="musicControlsButton" onClick={playClicked}><PlayIcon /></button>
-      }
+      <div id="musicPlayerButtons">
+        <button className="musicControlsButton" onClick={prevClicked}><PrevIcon /></button>
+        {playing? 
+          <button className="musicControlsButton" onClick={pauseClicked}><PauseIcon /></button>
+        : 
+          <button className="musicControlsButton" onClick={playClicked}><PlayIcon /></button>
+        }
+        <button className="musicControlsButton" onClick={nextClicked}><NextIcon /></button>
+      </div>
       {playerRef.current && 
         <div>
           <input 
